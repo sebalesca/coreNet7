c5c06d60191ef01ce2371ca8c9f1ab918b39218f
sebastian <sebalesca83o@gmail.com>
2023-04-11 09:00:43 -0300

    repositorios y contenedor de trabajo

---
+++ BlogCore/BlogCoreAccesoDatos/Data/Repository/ContenedorTrabajo.cs
@@ -0,0 +1,32 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Data;
+using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository
+{
+    public class ContenedorTrabajo : IContenedorTrabajo
+    {
+        private readonly ApplicationDbContext _db;
+        public ContenedorTrabajo(ApplicationDbContext db)
+        {
+            _db = db;
+            Categoria = new CategoriaRepositoy (_db);
+        }
+        public ICategoriaRepository Categoria { get; private set; } //private set significa que solo puede ser seteada dentro de la clase
+
+        public void Dispose()
+        {
+            _db.Dispose(); //libera recursos
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ BlogCore/BlogCoreAccesoDatos/Data/Repository/IRepository/ICategoriaRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BlogCore.AccesoDatos.Data.Repository.IRepository
+{
+    public interface ICategoriaRepository:IRepository<Categoria>
+    {
+        IEnumerable<SelectListItem> GetListaCategorias();
+        void Update (Categoria categoria);
+    }
+}
