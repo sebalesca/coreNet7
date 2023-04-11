78daef7f8a42bcb4f7f3dcb67a33064cb437130b
sebastian <sebalesca83o@gmail.com>
2023-04-11 09:55:06 -0300

    controlador categoria

---
+++ BlogCore/BlogCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,33 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        //en vez de usar el contexto de usa el contenedor de trabajo
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        private readonly ApplicationDbContext _context;
+        public CategoriasController(IContenedorTrabajo contenedorTrabajo,ApplicationDbContext context)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+            _context= context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region llamadas a la api
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Categoria.GetAll() });
+        }
+        #endregion
+    }
+}
