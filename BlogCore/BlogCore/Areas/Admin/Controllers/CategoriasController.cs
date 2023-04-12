0e13b054f03c1a19989e28007954c63a235f839f
sebastian <sebalesca83o@gmail.com>
2023-04-12 11:21:35 -0300

    Editar , borrar categoria

--- BlogCore/BlogCore/Areas/Admin/Controllers/CategoriasController.cs
+++ BlogCore/BlogCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -1,5 +1,6 @@
 ﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
 using BlogCore.Data;
+using BlogCore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogCore.Areas.Admin.Controllers
@@ -27,11 +28,62 @@ namespace BlogCore.Areas.Admin.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _contenedorTrabajo.Categoria.Add(categoria);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Categoria categoria = new Categoria();
+            categoria = _contenedorTrabajo.Categoria.Get(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _contenedorTrabajo.Categoria.Update(categoria);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+       
+
         #region llamadas a la api
         [HttpGet]
         public IActionResult GetAll()
         {
             return Json(new { data = _contenedorTrabajo.Categoria.GetAll() });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Categoria objFromDb = new Categoria();
+            objFromDb = _contenedorTrabajo.Categoria.Get(id);
+            if (objFromDb == null) {
+                return Json(new { succes = false, message = "Error borrado categoría" });
+            }
+            _contenedorTrabajo.Categoria.Remove(objFromDb);
+            _contenedorTrabajo.Save();
+            return Json(new {success=true,message="Borrado con Exito"});
         }
         #endregion
     }
--- BlogCore/BlogCore/wwwroot/js/categoria.js
+++ BlogCore/BlogCore/wwwroot/js/categoria.js
@@ -17,16 +17,16 @@ function cargarDataTable() {
             {
                 "data": "id",
                 "render": function (data) {
-                    return `<div> class="text-center">
-                                <a href="/Admin/Categorias/Edit/${data}" class="btn btn-sucess text-white" style="cursor:pointer; width:100%;">
-                                    <i class="far fa-edit"></i> Editar
+                    return `<div class="text-center">
+                                <a href="/Admin/Categorias/Edit/${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                                <i class="far fa-edit"></i>Editar
                                 </a>
                                 &nbsp;
-                                 <a onclick="Delete('/Admin/Categorias/Delete/${data}'" class="btn btn-danger text-white" style="cursor:pointer; width:100%;">
-                                    <i class="far fa-trash-alt"></i>Borrar
+                                <a onclick=Delete("/Admin/Categorias/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
+                                <i class="far fa-trash-alt"></i>Borrar
                                 </a>
                             </div>
-                    `;
+                            `;
                 },"width":"30%"
             }
         ],
@@ -52,4 +52,29 @@ function cargarDataTable() {
 
         }
     })
+}
+function Delete(url) {
+    swal({
+        title: "Esta seguro de borrar?",
+        text: "Este contenido no se puede recuperar!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Si, borrar!",
+        closeOnconfirm: true
+    }, function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message);
+                    dataTable.ajax.reload();
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        });
+    });
 }
